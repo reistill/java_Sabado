fd10520a7f412e2a4e77f63a4c2e88e19d6e95ab
reistill <joelreistill@gmail.com>
2024-08-31 11:39:28 -0300

    enunciado

---
+++ mentols.java
@@ -0,0 +1,29 @@
+//Fazer uma classe em Java que leia um nome (String) do teclado, e depois liste letra por letra, cada letra em uma linha diferente, o nome informado antes. Exemplo:
+
+//Ana Clara fica assim:
+
+//A
+
+//n
+
+//a
+
+ 
+
+//C
+
+//l
+
+//a
+
+//r
+
+//a
+
+//Lembrem que uma String, já é um Array...
+
+public class mentols {
+   public static void main(String[] args) {
+    
+   } 
+}
